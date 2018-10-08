30b7d6f03c5b14277a5b1a8b1b1017ef5f6ef20b
Tomasz <tomalek0@tlen.pl>
2018-10-08 21:21:53 +0200

    Initial commit

---
+++ src/main/java/com/Forum/Main.java
@@ -0,0 +1,17 @@
+package com.Forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by Tomal on 2017-05-13.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+
+}
